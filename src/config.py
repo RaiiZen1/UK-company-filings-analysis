5ec16bf42b2e8169aee9af4b4c400fc2d1929726
RaiiZen1 <shortfunnyvideos1@gmail.com>
2024-04-16 12:01:45 +0200

    Update config.py with additional search terms for plastic tax

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 API_KEY = "3ef1fc14-2fef-4400-ae57-bfb43b8103d0"
 BASE_URL = "https://api.companieshouse.gov.uk"
-COMPANY_NUMBERS = ["01087941", "04168334", "02019274"]
 TESSERACT_PATH = r"S:\Users\Hiwi\Plastic Tax\Program Files\Tesseract\tesseract.exe"
+COMPANY_NUMBERS = ["01087941", "04168334", "02019274"]
+SEARCH_TERMS = ["plastic", "plastic tax", "plastic levy", "environmental tax"]
