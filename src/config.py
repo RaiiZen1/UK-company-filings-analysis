93c4c68c763eefc8088895ecbe9e6888fc306da6
RaiiZen1 <116830141+RaiiZen1@users.noreply.github.com>
2024-05-29 10:55:35 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -4,10 +4,15 @@ API_KEY = "3ef1fc14-2fef-4400-ae57-bfb43b8103d0"
 BASE_URL = "https://api.companieshouse.gov.uk"
 TESSERACT_PATH = r"S:\Users\Hiwi\Plastic Tax\Program Files\Tesseract\tesseract.exe"
 
-SEARCH_TERMS = ["plastic", "plastic tax", "plastic levy", "environmental tax"]
-DOWNLOAD_FINANCIALS = True
-OCR_PDFS = True
-ANALYZE_PDFS = True
+SEARCH_TERMS = [
+    "plastic",
+    "plastic tax",
+    "plastic levy",
+    "environmental tax",
+]  # Edit this
+DOWNLOAD_FINANCIALS = False  # Edit this
+OCR_PDFS = False  # Edit this
+ANALYZE_PDFS = True  # Edit this
 
 try:
     COMPANY_NUMBERS = pd.read_csv("./data/company_ids.csv")["company_ids"].tolist()
